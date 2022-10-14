3191ff11f61d5f1d17fb140c75eb20f4b85d849e
XuHang <wayne_xuhang@163.com>
2022-10-14 11:42:59 +0800

    添加一些注释

--- index.js
+++ index.js
@@ -13,16 +13,15 @@ let exec = child_process.exec;
 const systemType = os.type();
 
 // 菜单模版
-
 const appMenuTemplate = [
     {
         label: 'Projects',
-        submenu: [
+        submenu: [ // 子菜单
             {
-                label: 'New Project',
-                accelerator: 'Cmd+N',
+                label: 'New Project', // 显示名称
+                accelerator: 'Cmd+N', // 快捷键
                 click: () => {
-                    win.webContents.send('new-project-dialog', {  })
+                    win.webContents.send('new-project-dialog', {  }) // 事件
                 }
             }, {
                 label: 'Rename Project'
@@ -46,6 +45,7 @@ const appMenuTemplate = [
     }
 ]
 
+// macOS上Dock菜单
 const dockMenuTempalte = [
     {
         label: 'New Window',
@@ -86,6 +86,8 @@ if (systemType === 'Darwin') {
 app.commandLine.appendSwitch('--disable-http-cache')
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
+// 绑定自定义协议。 注：MacOS和Linux下，自定义协议只有在打包之后才会生效，在开发模式下通过命令行启动不会生效。
+app.removeAsDefaultProtocolClient('fily')
 if (process.defaultApp) {
     if (process.argv.length >= 2) {
       app.setAsDefaultProtocolClient('fily', process.execPath, [path.resolve(process.argv[1])])
@@ -94,7 +96,7 @@ if (process.defaultApp) {
     app.setAsDefaultProtocolClient('fily')
 }
 
-
+// 单实例运行
 const gotTheLock = app.requestSingleInstanceLock()
 if (!gotTheLock) {
     app.quit()
@@ -105,7 +107,7 @@ if (!gotTheLock) {
                 if (win.isMinimized()) win.restore()
                 win.focus()
                 // windows上自定义协议是通过第二个实例启动的
-                if (systemType === 'Window_NT') {
+                if (systemType === 'Windows_NT') {
                     let lastCommand = commandLine[commandLine.length - 1];
                     if (lastCommand.indexOf('fily://') == 0) {
                         openUrl(lastCommand)
@@ -117,31 +119,34 @@ if (!gotTheLock) {
     app.on('ready', createWindow);
 }
 
+// 自定义协议事件处理回调
 app.on('open-url', (event, url) => {
     console.log('url=', url);
     openUrl(url)
 })
 
-// 当全部窗口关闭时退出
+// 当全部窗口关闭事件触发时退出
 app.on('window-all-closed', () => {
     console.log('所有窗口关闭')
     app.quit();
 })
 
+// 使用系统默认应用打开文件
 ipcMain.on('open-file-with-os-program', (event, data) => {
-    if (data['_type'] && data['_type'] === 'explorer') {
-        if (systemType === 'Window_NT') {
+    if (data['_type'] && data['_type'] === 'explorer') { // 文件夹
+        if (systemType === 'Windows_NT') {
             exec('explorer.exe /select,' + data['_path'])
         } else if (systemType === 'Darwin') {
             shell.showItemInFolder(data['_path'])
         }
         
-    } else {
+    } else { // 文件
         shell.openPath(data['_path'])
     }
     // exec('open "' + data['_path'] + '"')
 })
 
+// 打开指定页面
 ipcMain.on('change-view', (event, data) => {
     let view = data['_page'];
     if (!!view) {
@@ -155,6 +160,7 @@ ipcMain.on('change-view', (event, data) => {
 //     BrowserWindow.fromWebContents(event.sender).setIgnoreMouseEvents(...args)
 // })
 
+// 聊天窗口
 let chatWindow
 ipcMain.on('load-chat-room', () => {
     let scrWid = screen.getPrimaryDisplay().workAreaSize.width;
@@ -192,21 +198,7 @@ ipcMain.on('load-chat-room', () => {
     });
 });
 
-
-
-ipcMain.handle('dark-mode:toggle', () => {
-    if (nativeTheme.shouldUseDarkColors) {
-      nativeTheme.themeSource = 'light'
-    } else {
-      nativeTheme.themeSource = 'dark'
-    }
-    return nativeTheme.shouldUseDarkColors
-})
-
-ipcMain.handle('dark-mode:system', () => {
-    nativeTheme.themeSource = 'system'
-})
-
+// 窗口控制事件
 ipcMain.on('window-ctrl', (event, data) => {
     let opt = data['_op'];
     switch (opt) {
@@ -223,6 +215,7 @@ ipcMain.on('window-ctrl', (event, data) => {
     }
 })
 
+// 消息通知事件
 ipcMain.on('show-notification', (event, data) => {
     const notification = {
         appId: "com.xuhang.electron.demo",
@@ -233,22 +226,7 @@ ipcMain.on('show-notification', (event, data) => {
     new Notification(notification).show()
 })
 
-// ipcMain.on('drag-start', (event, data) => {
-//     event.sender.startDrag({
-//         file: data.filePath,
-//         icon: data.fileIcon
-//     })
-//     return true
-// })
-
-// ipcMain.on('ondragstart', (event, filePath) => {
-//     event.sender.startDrag({
-//       file: path.join(__dirname, filePath),
-//       icon: './resource/icos/pre.ico',
-//     })
-// })
-
-
+// 关闭窗口事件
 ipcMain.on('close-window', (event, data) => {
     console.log('closing window ' + data['_src']);
 
@@ -272,18 +250,9 @@ ipcMain.on('close-window', (event, data) => {
 });
 
 
-// 根据缩略图按钮切换图片
+// 根据缩略图按钮切换项目
 let thumbImageIndex = 1;
 const thumbImagePath = path.join(__dirname, './resource/pics')
-
-function openUrl(url) {
-    projectName = url.substring(url.indexOf('//') + 2);
-    if (projectName.indexOf('/') === projectName.length - 1) {
-        projectName = projectName.substring(0, projectName.length - 1);
-    }
-    console.log('projectName =', projectName);
-    win.webContents.send('focus-on-project', {'projectName': projectName});
-}
 
 function switchThumbImage(delta) {
     let imgs = fs.readdirSync(thumbImagePath);
@@ -302,7 +271,6 @@ function switchThumbImage(delta) {
 }
 
 function createWindow() {
-
     // 读取package.json文件中的属性
     var package = require('./package.json')
     console.log('starting ' + package.name + ' ' + package.version)
@@ -332,9 +300,9 @@ function createWindow() {
         // resizable: true,        // 是否可调整大小
         transparent: true, //是否透明
         webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-            contextIsolation: false,
+            nodeIntegration: true, // 允许html页面上的javascipt代码访问nodejs 环境api代码的能力（与node集成的意思）
+            enableRemoteModule: true, // 在渲染进程中使用remote模块，在高版本Electorn中还需要安装@electron/remote模块，然后调用initialize()和enable()方法。
+            contextIsolation: false, // 阻止网站访问 Electron 的内部组件 和 预加载脚本可访问的高等级权限的API。禁用：网页js可以访问preload脚步的window全局对象；启用：无法访问全局window对象，通过contextBridge暴露的才能访问。
             preload: './script/preload.js'
         }
     })
@@ -342,58 +310,41 @@ function createWindow() {
     remoteMain.enable(win.webContents);
 
     if (systemType === "Darwin") {
+        // macOS 上Dock图标
         app.dock.setIcon(path.join(__dirname, './img/icon_512x512.png'));
         // 设置Dock菜单
         const dockMenu = Menu.buildFromTemplate(dockMenuTempalte);
         app.dock.setMenu(dockMenu);
-        // win.on('blur', function() {
-        //     const badgeString = app.dock.getBadge();
-        //     if (badgeString === '') {
-        //         app.dock.setBadge('1');
-        //     } else {
-        //         app.docker.setBadge((parseInt(badgeString)+1).toString());
-        //     }
-        // });
+        // macOS 上Dock图标上显示数字小红点
+        win.on('blur', function() {
+            const badgeString = app.dock.getBadge();
+            if (badgeString === '') {
+                app.dock.setBadge('1');
+            } else {
+                app.dock.setBadge((parseInt(badgeString)+1).toString());
+            }
+            // Dock 图标跳动一次
+            app.dock.bounce('informational');
+        });
     }
-
-    // win.setIgnoreMouseEvents(true)
-    // win.maximize()
-    // win.setAlwaysOnTop(true, "floating");
-    // win.setVisibleOnAllWorkspaces(true);
-
-    // 读取注册表peopleId && 注册webim
-    // webimReg(readPeopleId);
-
-    // 加载页面3
-    // win.loadFile('./page/projectRelatedFiles.html')
+    // 加载要现实点页面
     win.loadFile('./page/projectRelatedFiles.html')
 
-    // Windows 任务栏图标菜单
+    // 任务栏图标菜单 windows在右下角，macOS在右上角
     // tray = new Tray(path.join(__dirname, "./img/fily.png"));
     const trayImage = nativeImage.createFromPath(path.join(__dirname, "./img/icons.iconset/file20Template.png"));
     trayImage.resize({ width: 16, height: 16 })
     trayImage.setTemplateImage(true);
     tray = new Tray(trayImage);
+    // 任务栏图标的右键菜单
     const contextMenu = Menu.buildFromTemplate([
         { label: '退出', click: function() { app.quit() } }
     ])
+    // 任务栏图标的鼠标hover提示
     tray.setToolTip('Fily - File manager for you!')
     tray.setContextMenu(contextMenu)
 
-
-    // WIN8 & WIN10 不支持原生window.Notification，调用第三方通知窗口
-    // addNotificationListner();
-
-    /* win.setThumbarButtons([
-        {
-            tooltip: 'button1',
-            icon: path.join(__dirname, './img/music-room.png'),
-            click() { console.log('button1 clicked') }
-        }
-    ]); */
-
-    // win.setThumbnailToolTip('正在播放...');
-
+    // 预览窗口功能，only windows
     win.setThumbarButtons([{
         tooltip: '上一张',
         icon: path.join(__dirname, './resource/icos/pre.ico'),
@@ -405,17 +356,28 @@ function createWindow() {
     }])
 
     // win.setAlwaysOnTop(true, 'screen-saver');
-
+    // 全局快捷键
     globalShortcut.register('Ctrl+W', () => {
         win = null;
         app.quit();
     });
-    win.webContents.toggleDevTools()
+    // 打开开发者工具
+    //win.webContents.toggleDevTools()
     // 添加window关闭触发事件
     win.on('close', () => {
         win = null;
         app.quit();
     });
 
+    // 图标上显示进度条
     win.setProgressBar(0.7);
+}
+
+function openUrl(url) {
+    projectName = url.substring(url.indexOf('//') + 2);
+    if (projectName.indexOf('/') === projectName.length - 1) {
+        projectName = projectName.substring(0, projectName.length - 1);
+    }
+    console.log('projectName =', projectName);
+    win.webContents.send('focus-on-project', {'projectName': projectName});
 }
